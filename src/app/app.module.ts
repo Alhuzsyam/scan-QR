63f843daa41b47d81605832801b988c2f4c31b07
ALFI ALHUZWIRI SYAM <223970007@bukopin.co.id>
2023-11-23 15:20:12 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxScannerQrcodeModule} from 'ngx-scanner-qrcode';
+import { ScannerComponent } from './scanner/scanner.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxScannerQrcodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
